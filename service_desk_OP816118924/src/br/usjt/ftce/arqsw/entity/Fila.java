0056e6114f40707af25a65b6a8fdc78413468fde
arqdsis <arqdsis@10.71.18.10>
2018-02-22 19:27:08 -0300

    first commit

---
+++ service_desk_OP816118924/src/br/usjt/ftce/arqsw/entity/Fila.java
@@ -0,0 +1,23 @@
+package br.usjt.ftce.arqsw.entity;
+
+public class Fila {
+	private int id;
+	private String nome;
+
+	public String getNome() {
+		return nome;
+	}
+
+	public void setNome(String nome) {
+		this.nome = nome;
+	}
+
+	public int getId() {
+		return id;
+	}
+
+	public void setId(int id) {
+		this.id = id;
+	}
+
+}
