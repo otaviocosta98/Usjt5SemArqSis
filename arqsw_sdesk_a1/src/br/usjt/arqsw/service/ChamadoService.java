294a91e6f14770b50ddfe60f69b6145e94793513
otaviocosta98 <cax_biro@hotmail.com>
2018-03-23 15:23:11 -0300

    Atulizado salvarChamado hibernate

--- arqsw_sdesk_a1/src/br/usjt/arqsw/service/ChamadoService.java
+++ arqsw_sdesk_a1/src/br/usjt/arqsw/service/ChamadoService.java
@@ -3,6 +3,7 @@ package br.usjt.arqsw.service;
 import java.io.IOException;
 import java.util.Date;
 import java.util.List;
+import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
